cf0f71ab9f4ed1ad50bba6fa9acba78ebc349a7d
Kelvin Njiraini <njirainikelvin@gmail.com>
2023-06-26 15:07:45 +0300

    config file changed for supabase installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/supabase'],
+    devtools: true,
+    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
 });
