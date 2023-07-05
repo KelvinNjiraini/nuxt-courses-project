07fd4b54bef86496bbe6a37ab125d6db059b6903
Kelvin Njiraini <njirainikelvin@gmail.com>
2023-07-05 10:49:08 +0300

    feat:lesson progress model added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ import { defineNuxtConfig } from 'nuxt/config';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     devtools: true,
-    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/supabase'],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@vueuse/nuxt',
+        '@nuxtjs/supabase',
+        '@pinia/nuxt',
+    ],
 });
